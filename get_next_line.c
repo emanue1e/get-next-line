888372b2f09646638876cfdb2fa5b293420fe280
Emanuele Torrefiel <emanuele@Emanueles-MacBook-Pro.local>
2021-12-11 17:37:32 +1030

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,33 @@
 /*   By: emanuele <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 06:31:20 by emanuele          #+#    #+#             */
-/*   Updated: 2021/12/11 17:28:59 by emanuele         ###   ########.fr       */
+/*   Updated: 2021/12/11 17:36:40 by emanuele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char	*ft_tail(char *tail, int *n)
+{
+	char	*str;
+
+	str = ft_substr(tail, *n + 1, ft_strlen(tail));
+	free(tail);
+	return (str);
+}
+
+static char	*ft_line(char *tail, int *n)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	while (tail[i] != '\n' && tail[i])
+		i++;
+	line = ft_substr(tail, 0, i + 1);
+	*n = i;
+	return (line);
+}
 
 static char	*ft_read(int fd, char *tail)
 {
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: emanuele <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 17:25:24 by emanuele          #+#    #+#             */
-/*   Updated: 2021/12/11 17:25:26 by emanuele         ###   ########.fr       */
+/*   Updated: 2021/12/11 17:37:27 by emanuele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ int ft_strlen(char *str);
 char *ft_strchr(const char *str, int c);
 char *ft_substr(char *s, int start, int len);
 char *ft_strjoin(char *s1, char *s2);
-char *ft_tail(char *tail, int *n);
-char *ft_line(char *tail, int *n);
 char *get_next_line(int fd);
 
 #endif
